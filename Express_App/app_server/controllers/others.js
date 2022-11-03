7744adfd885dbacc543559fe09adb09565e9d305
Deirdre-cyber <Deirdre.Lee@students.ittralee.ie>
2022-11-03 15:12:33 +0000

    Data in controller, main and myRentals

--- Express_App/app_server/controllers/others.js
+++
@@ -1,7 +0,0 @@
-/* Register */
-const register = function (req, res) {
-    res.render('index', { title: 'Register' });
-};
-module.exports = {
-    register
-};
--- Express_App/app_server/routes/index.js
+++ Express_App/app_server/routes/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const ctrlRentals = require('../controllers/rentals');
-const ctrlOthers = require('../controllers/others');
+const ctrlOthers = require('../controllers/users');
 
-/* Locations pages */
-router.get('/', ctrlRentals.rentalsList);
-router.get('/login', ctrlRentals.login);
-router.get('/myaccount', ctrlRentals.myAccount);
+/* Rentals pages */
+router.get('/', ctrlRentals.rentals);
+router.get('/rentals', ctrlRentals.myRentals);
+
 
 /* Other pages */
 router.get('/register', ctrlOthers.register);
+router.get('/login', ctrlOthers.login);
+router.get('/myaccount', ctrlOthers.myAccount);
 
 module.exports = router;
