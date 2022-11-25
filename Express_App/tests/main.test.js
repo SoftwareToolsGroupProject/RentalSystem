3c876c33a2e4d8c9f92b97ed6e0c6a14d48a00c0
Deirdre-cyber <Deirdre.Lee@students.ittralee.ie>
2022-11-25 13:05:14 +0000

    Added database tests to main.test.js

--- Express_App/tests/main.test.js
+++ Express_App/tests/main.test.js
@@ -2,23 +2,66 @@
  * @jest-environment jsdom
  */
 
-require('../public/javascripts/main.js');
+'use strict'
 
-describe('Test /main', () => {
-    before('before', () => {
-        console.log('Ran before all the test suites');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//Database testing
+describe('Member CRUD', () => {
+
+    let connection;
+    let db;
+    const members = mongoose.model("test_"+process.env.COLLECTION,mongoose.Schema({
+        MemberID: String,
+        Email: String
+    }));
+
+    beforeAll(async () => {
+
+        connection = await mongoose.connect('mongodb://localhost:27017/test_'+process.env.DATABASE,{useNewUrlParser: true, useUnifiedTopology: true });
+        db = mongoose.connection;
+        const collection = process.env.COLLECTION;
+        await db.createCollection(collection);
     });
 
-    after('after', () => {
-        console.log('Ran after all the test suites');
+    afterAll(async () => {
+        const collection = "test_" + process.env.COLLECTION;
+        await db.dropCollection(collection);
+        await db.dropDatabase();
+        await db.close();
+       // await connection.end();
     });
 
-    beforeEach('beforeEach', () => {
-        console.log('Ran before EACH test suite');
-    });
+    /*test("Get Member POST /members", async () => {
+        const response = await members.create({
+            MemberID: process.env.MEMBERID,
+            Email: process.env.EMAIL
+        });
+        await response.save();
+        expect(response.memId).toBe(process.env.MEMBERID);
+    });*/
 
-    afterEach('afterEach', () => {
-        console.log('Ran after EACH test suite');
-    });
+    test("All Members GET /members", async () => {
+        const response = await members.find({});
+        
+        expect(response.length).toBe(0);
+     });
+
+    /* test("Update Member PUT /members/:id", async () => {
+        const response = await members.updateOne({MemberID: process.env.MEMBERID},{Email: process.env.EMAIL_ALT});
+        expect(response.ok).toBeTruthy();
+      });
+
+      test("Member update is correct", async () => {
+      const responseTwo = await members.findOne({MemberID: process.env.MEMBERID});
+      expect(responseTwo.Email).toBe(process.env.EMAIL_ALT);
+      });
+
+      test("Delete Memebr DELETE /members/:id", async() => {
+        const response = await members.deleteOne({MemberID: process.env.MEMBERID});
+        expect(response.ok).toBe(1);
+      });*/
 
 });
+
--- Express_App/tests/myaccount.test.js
+++
@@ -1,9 +0,0 @@
-/**
- * @jest-environment jsdom
- */
-
-require('../public/javascripts/myaccount.js');
-
- test('update details function', () => {
-     expect(updateDetails()).toBe("Your account details have been updated");
- });
